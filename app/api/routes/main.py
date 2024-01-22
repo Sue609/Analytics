cf274b8ab0fc9e9055275637750ceaae43ecdf5c
Sue609 <122805048+Sue609@users.noreply.github.com>
2024-01-22 20:04:51 +0300

    defining the create_user route

--- app/api/routes/main.py
+++ app/api/routes/main.py
@@ -1,6 +1,9 @@
 # main.py
 from flask import Flask, render_template, redirect, url_for, request
-from app.api.Authentication.auth import db, argon2, login_manager, Users, create_user_and_login
+from auth import db, argon2, login_manager, Users, create_user_and_login
+from flask_login import login_user
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.db"
@@ -11,19 +14,26 @@ login_manager.init_app(app)
 
 
 
-@app.route('/signup', methods=['POST'])
-def signup():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    Defining a route that allows a user to login to their accounts.
+    """
     if request.method == 'POST':
-        username = request.form['username']
         email = request.form['email']
         password = request.form['password']
-
-        create_user_and_login(username, email, password)
-
-        return redirect(url_for('dashboard'))
-
-    return render_template('signup.html')
-
+        
+        user = Users.query.filter_by(email=email).first()
+        
+        if user is not None and argon2.check_password_hash(user.password, password):
+            # if password is correct, log the user in
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        
+        error_message = "Invalid email or password. Please try again."
+        return render_template("/api/templates/index.html", error=error_message)
+        
+    return render_template('/api/templates/index.html')
 
 if __name__ == "__main__":
     with app.app_context():
