2bf73dc7d99c475c5ada7041f14d15451916a8fe
Sue609 <122805048+Sue609@users.noreply.github.com>
2024-01-24 15:14:55 +0300

    implementing the home route

--- app/main.py
+++ app/main.py
@@ -24,7 +24,37 @@ def home():
     Route for the home page.
     """
     return render_template('home.html')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    """
+    Defining a route that allows users to sign up and create a new account.
+    Check if the email is already registered.
+    Hash tha password using argon2.
+    create new user and add them to the db.
+    """
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        
+        user = Users.query.filter_by(email=email).first()
+        if user:
+            error_message = "Email already registred. Please use a different email"
+            return render_template("index.html", error=error_message)
+        
+        hashed_pwd = argon2.generate_password_hash(password)
+        new_user = Users(username=username, email=email, password=hashed_pwd)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for('dashboard'))
+        except IntegrityError:
+            db.session.rollback()
+            error_message = "An error occured while creating the user. Please try again"
+            return render_template("index.html", error=error_message)
 
+    return render_template('index.html')
 
 
 @app.route('/dashboard')
