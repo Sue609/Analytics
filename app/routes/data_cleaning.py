ea881b0ee2d68fd590d61962941ae3d15cee361d
Sue609 <122805048+Sue609@users.noreply.github.com>
2024-02-29 08:37:18 +0300

    Implementing the data cleaning cleaning webpage and adding the download button.

--- app/routes/data_cleaning.py
+++ app/routes/data_cleaning.py
@@ -1,19 +1,14 @@
-#!/usr/bin/env python3
-"""
-This module introduces the analyze route.
-"""
-from flask import render_template, request, redirect, url_for, current_app
+from flask import render_template, request, redirect, url_for, current_app, jsonify
 from flask.blueprints import Blueprint
 import pandas as pd
 import os
 from werkzeug.utils import secure_filename
 from routes.upload import allowed_file
 
-
-clean_data_app = Blueprint('clean_data_app', __name__, template_folder='templates')
+clean_data_app = Blueprint('clean_data_app', __name__)
 
 @clean_data_app.route('/clean_data', methods=['GET', 'POST'])
-def data_cleaning():
+def clean_data():
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
@@ -24,11 +19,11 @@ def data_cleaning():
             return redirect(request.url)
         
         if file and allowed_file(file.filename):
+            # Save the uploaded file
             upload_folder = current_app.config['UPLOAD_FOLDER']
             os.makedirs(upload_folder, exist_ok=True)
             
             filename = secure_filename('data_cleaning_files.csv')
-
             filepath = os.path.join(upload_folder, filename)
             file.save(filepath)
             
@@ -38,14 +33,14 @@ def data_cleaning():
                 # Clean the data
                 cleaned_df = clean_data(df)
                 
+                # save the cleaned data to a new CSV file
                 cleaned_filepath = os.path.join(upload_folder, 'cleaned_data.csv')
                 cleaned_df.to_csv(cleaned_filepath, index=False)
-
-                return render_template('data_cleaning.html', cleaned_data=cleaned_df.to_html(), filename=cleaned_filepath)
+    
+                # Render the clean_data.html template with the uploaded and cleaned data
+                return render_template('clean_data.html', cleaned_data=cleaned_df.to_html(), filename=cleaned_filepath)
 
             except Exception as e:
                 return f'Error reading or cleaning CSV file: {e}'
 
-    return render_template('data_cleaning.html')
-
-
+    return render_template('clean_data.html')
